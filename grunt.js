35bdf1c59f3170b41993f6f214db6d401c3cd2ce
Tim Koch-Grunberg <tim.theodor@gmail.com>
2012-05-09 08:35:03 +0100

    grunt config

---
+++ grunt.js
@@ -0,0 +1,58 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    meta: {
+      version: '0.1.0',
+      banner: '/*! Edward - v<%= meta.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '* https://github.com/timkg/edward/\n' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+        'Tim Theodor Koch-Grunberg; Licensed MIT */'
+    },
+    lint: {
+      files: ['grunt.js', 'src/**/*.js', 'test/**/*.js']
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    concat: {
+      dist: {
+        src: ['src/edward.core.js', 'src/edward.utils.js', 'src/edward.point.js', 'src/edward.circle.js'],
+        dest: 'dist/edward.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+        dest: 'dist/edward.min.js'
+      }
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint qunit'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {}
+    },
+    uglify: {}
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint qunit concat min');
+
+};
