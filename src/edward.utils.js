3fc0c1aa1ec2456c7e2b22a8dcb5e672f1d3cad1
Tim Koch-Grunberg <tim.theodor@gmail.com>
2012-07-08 12:17:46 +0100

    added a GUID generator to utils

--- src/edward.utils.js
+++ src/edward.utils.js
@@ -103,6 +103,16 @@ edward.utils = (function(){
             return color;
         }
     }
+ 
+ 
+    // from http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
+    function guidGenerator() {
+        var S4 = function() {
+            return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+        };
+    
+        return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+    }
     
     
     return {
@@ -112,7 +122,8 @@ edward.utils = (function(){
         distanceBetween: distanceBetween,
         setupRequestAnimationFrame: setupRequestAnimationFrame,
         colorToRGB: colorToRGB,
-        parseColor: parseColor
+        parseColor: parseColor,
+        guidGenerator: guidGenerator
     };
     
     
